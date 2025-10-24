e8f6965fa0d53bdc96f64ee91d5455bb6fa6647e
AndreiCasita <andreicasita3301@gmail.com>
2025-10-24 20:40:21 -0300

    Add files via upload

---
+++ Atividade javascript/script.js
@@ -0,0 +1,21 @@
+function alterartexto(){
+   document.getElementById("titulo").innerText = "texto mod";
+   alert("Popup");
+}
+
+let idade = "19";
+
+console.log("idade:",idade);
+
+idade = "20";
+
+console.log("idade:",idade);
+
+const produto = {
+    nome: "Motorcycle",
+    valor: 18500.00
+}
+
+produto.nome = "Car";
+
+console.log(produto.nome);
